89a8eed78de073c954fe909f09ce193a9a572e5d
Guillaume CLEMENT <guillaume.clement@koovea.com>
2022-03-26 21:57:24 +0100

    add item group for added item

--- src/main/java/net/fabricmc/hellomod/HelloMod.java
+++ src/main/java/net/fabricmc/hellomod/HelloMod.java
@@ -1,8 +1,10 @@
 package net.fabricmc.hellomod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -10,19 +12,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HelloMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("hellomod");
 
-	public static final SoundOnUseItem TEST_ITEM = new SoundOnUseItem(new FabricItemSettings().group(ItemGroup.MISC));
+	// Creating items
+	// TODO : damageable, take durability damage on une
+	public static final SoundOnUseItem TEST_ITEM = new SoundOnUseItem(new FabricItemSettings());
+
+	// Creating item groups
+	public static final ItemGroup GENERAL_GROUP = FabricItemGroupBuilder.create(new Identifier("hellomod", "general"))
+			.icon(() -> new ItemStack(HelloMod.TEST_ITEM))
+			.appendItems(stacks -> {
+				stacks.add(new ItemStack(HelloMod.TEST_ITEM));
+			})
+			.build();
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		Registry.register(Registry.ITEM, new Identifier("hellomod", "test_item"), TEST_ITEM);
+		Registry.register(Registry.ITEM, new Identifier("hellomod", "test_item"), HelloMod.TEST_ITEM);
 
 		LOGGER.info("Hello Fabric world!");
 	}
