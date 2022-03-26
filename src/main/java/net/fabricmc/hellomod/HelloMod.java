e4fcf7915ec1b40d520e969086dff2f3724b484f
Guillaume CLEMENT <guillaume.clement@koovea.com>
2022-03-26 21:20:56 +0100

    change example => hellomod

--- src/main/java/net/fabricmc/hellomod/HelloMod.java
+++ src/main/java/net/fabricmc/hellomod/HelloMod.java
@@ -1,20 +1,29 @@
-package net.fabricmc.example;
+package net.fabricmc.hellomod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class HelloMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("hellomod");
+
+	public static final Item TEST_ITEM = new Item(new FabricItemSettings().group(ItemGroup.MISC));
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		Registry.register(Registry.ITEM, new Identifier("hellomod", "test_item"), TEST_ITEM);
 
 		LOGGER.info("Hello Fabric world!");
 	}
