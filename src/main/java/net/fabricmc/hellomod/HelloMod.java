b2e6515faf74b41e9e92dac7c9b1a0958a6e05d2
Guillaume CLEMENT <guillaume.clement@koovea.com>
2022-03-27 16:39:59 +0200

    ...

--- src/main/java/net/fabricmc/hellomod/HelloMod.java
+++ src/main/java/net/fabricmc/hellomod/HelloMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-
+import net.fabricmc.hellomod.items.SoundOnUseItem;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
